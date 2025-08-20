d4cf152732b160eb1bc3b2a0e55f96ab38ea2e21
CliffeAbudo <fortuneabudo@gmail.com>
2025-08-20 20:55:43 +0300

    Javascript Lesson one

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+let name = 'John';
+let surname = 'Doe';
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+console.log(23+97);
+console.log( (4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+console.log(percentage);
