e210dec039a31ab47856626bc572ba188e24e63f
CliffeAbudo <fortuneabudo@gmail.com>
2025-08-31 09:12:08 +0300

    small changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,27 @@
+/*
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
+
+btn.addEventListener("click", updateBtn);
+
+function updateBtn() {
+  if (btn.textContent === "Start machine") {
+    btn.textContent = "Stop machine";
+    txt.textContent = "The machine has started!";
+  } else {
+    btn.textContent = "Start machine";
+    txt.textContent = "The machine is stopped.";
+  }
+}
+
+*/
+
+
+
+
+
+
+
 let name = 'John';
 let surname = 'Doe';
 
@@ -21,7 +45,33 @@ a = 20;
 let b = 7 * a;
 console.log(b);
 
+const myInt = 5;
+const myFloat =6.667;
+
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+
 const max = 57;
 const actual = max-13;
 const percentage = actual/max;
 console.log(percentage);
+
+const lotsOfDecimal = 1.7665849587;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
+
+let myNumber = "74";
+myNumber += 3;
+
+console.log(typeof myNumber);
+
+let num1 = 4;
+num1++;
+console.log(num1);
+
+
+let num2 = 10;
+--num2;
+console.log(num2)
