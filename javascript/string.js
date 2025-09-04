578d368d07ee9a34ed2e44f1d2c18f17c0540f21
CliffeAbudo <fortuneabudo@gmail.com>
2025-09-04 23:56:58 +0300

    string

---
+++ javascript/string.js
@@ -0,0 +1,23 @@
+//Javascript string methods
+
+//length
+let text = "qwerttyuiuiopp";
+let length = text.length;
+console.log(length);
+
+//Extracting string character methods
+// has 4 methods
+let text1 = "HELLO WORLD";
+let char = text1.charAt(0);
+console.log(char);
+
+let text2 = "HELLO WORLD";
+let char2 = text2.charCodeAt(0);
+console.log(char2);
+
+let text3 = "HELLO WORLD";
+let code = text3.codePointAt(0);
+console.log(code);
+
+const name = "W3Schools";
+let letter = name.at(2);
